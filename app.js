b0a0bbe8a22f5ece9cabefa51e5f6be873664b42
jcapoduri <jcapoduri@gmail.com>
2015-04-26 02:38:46 +0000

    pre-refactor for unittesting

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ var bodyParser =    require('body-parser');
 var app = 		      express();
 var router =        express.Router();
 var models = 	      require("./models");
-var controllers  =  require('./controllers');
-var routes  =       require('./routes');
+var routeMaps  =    require('./routes');
+var Sequelize =     require("sequelize");
+var env       =     process.env.NODE_ENV || "development";
+var config    =     require(__dirname + '/config/config.json')[env];
+var sequelize =     new Sequelize(config.database, config.username, config.password, config);
 
 app.set('port', process.env.PORT || 3000);
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
-models.sequelize.sync({force: false}).then(function () {
-  var paths = routes.set(router, models);
+models.init(sequelize);
+
+sequelize.sync({force: false}).then(function () {
+  var paths = routeMaps.set(router, models);
   app.use('/api', paths);
   var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
